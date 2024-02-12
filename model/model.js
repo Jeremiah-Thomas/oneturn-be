ec67da759b559bf8a3265d3c901c49f731fb125c
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2024-02-11 23:21:21 -0500

    added the rest of endpoints and app is functional

--- model/model.js
+++ model/model.js
@@ -1,47 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const afflictionSchema = new mongoose.Schema({
-    name: {
-        required: false,
-        type: String
-    },
-    max_duration: {
-        required: false,
-        type: Number
-    },
-    cur_duration: {
-        required: false,
-        type: Number
-    },
-    color: {
-        reqiured: false,
-        type: String
-    },
-    stacks: {
-        required: false,
-        type: Number
-    }
-
-})
-
-const monsterSchema = new mongoose.Schema({
-   afflictions: {
-    type: [afflictionSchema],
-    required: false
-   },
-   monster_name: {
+  name: {
+    required: false,
     type: String,
-    required: true
-   },
-   doom: {
-    type: Boolean,
-    required: false
-   },
-   abyssal_mal: {
-    type: Boolean,
-    required: false
-   }
-},{collection: 'monsters'})
+  },
+  max_duration: {
+    required: false,
+    type: Number,
+  },
+  cur_duration: {
+    required: false,
+    type: Number,
+  },
+  color: {
+    reqiured: false,
+    type: String,
+  },
+  stacks: {
+    required: false,
+    type: Number,
+  },
+});
 
+const monsterSchema = new mongoose.Schema(
+  {
+    afflictions: {
+      type: [afflictionSchema],
+      required: false,
+    },
+    monster_name: {
+      type: String,
+      required: true,
+    },
+    doom: {
+      type: Boolean,
+      required: false,
+    },
+    abyssal_mal: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  { collection: "monsters" }
+);
 
-module.exports = mongoose.model('Data', monsterSchema)
+module.exports = mongoose.model("Data", monsterSchema);
