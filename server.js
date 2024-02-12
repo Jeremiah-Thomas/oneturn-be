7a24abd8883614c58cc756036f5e7b4fcdf5bd03
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2024-02-11 23:34:38 -0500

    added the rest of endpoints and app is functional

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes/routes')
+const routes = require("./routes/routes");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const mongoString = process.env.MONGODB
+const mongoString = process.env.MONGODB;
 
-mongoose.connect(mongoString)
-const database = mongoose.connection
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
-database.once('connected', () => {
-    console.log('Connected')
-})
+database.on("error", (error) => {
+  console.log(error);
+});
+database.once("connected", () => {
+  console.log("Connected");
+});
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use('/api', routes)
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(express.json());
+app.use("/api", routes);
 
 app.listen(8000, () => {
-    console.log(8000)
-})
+  console.log(8000);
+});
