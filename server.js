541402a6f08d763a382e5507ed04237fba7dc5bc
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2024-02-11 23:42:32 -0500

    added the rest of endpoints and app is functional

--- server.js
+++ server.js
@@ -20,20 +20,7 @@ database.once("connected", () => {
 
 const app = express();
 
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(cors({ origin: "https://oneturn.netlify.app", credentials: true }));
 
 app.use(express.json());
 app.use("/api", routes);
